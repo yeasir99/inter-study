3615383a67ef8be7f2ced361af2fea18ec0c0028
Yeasir Ahamed <yahamed007@gmail.com>
2023-03-27 23:45:28 +0600

    add dark modde

--- client/src/App.js
+++ client/src/App.js
@@ -2,16 +2,40 @@ import { Route, Routes } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './components/Login';
 import Signup from './components/signup';
+import { useState, useEffect } from 'react';
 
 function App() {
+  const [theme, setTheme] = useState('light');
+  useEffect(() => {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+      setTheme('dark');
+    } else {
+      setTheme('light');
+    }
+  }, []);
+  useEffect(() => {
+    if (theme === 'dark') {
+      document.documentElement.classList.add('dark');
+    } else {
+      document.documentElement.classList.remove('dark');
+    }
+  }, [theme]);
+
+  const handleThemeSwitch = () => {
+    setTheme(theme === 'dark' ? 'light' : 'dark');
+  };
+
   return (
-    <div className="max-w-screen-xl mx-auto">
-      <header>Main header goes here</header>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/signin" element={<Login />} />
-        <Route path="/signup" element={<Signup />} />
-      </Routes>
+    <div className="bg-white dark:bg-gray-800">
+      <div className="max-w-screen-xl mx-auto">
+        <header>Main header goes here</header>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/signin" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/dashboard" element={<h1>dashboard</h1>} />
+        </Routes>
+      </div>
     </div>
   );
 }
